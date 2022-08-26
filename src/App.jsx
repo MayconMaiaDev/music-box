9ae1074a94b0bef5ab335256d20b4c3a30d4155b
Maycon Gomes <86531134+MayconMaiaDev@users.noreply.github.com>
2022-08-26 14:56:16 -0300

    Adicionando projeto
    Projeto music box

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Nav from "./components/Nav"; // Importando o componente Nav
+
+// Exporta e cria o componente App
+export default function App() {
+  return <Nav />;
+}
+
+// Exporta o componente App
+// export default App
